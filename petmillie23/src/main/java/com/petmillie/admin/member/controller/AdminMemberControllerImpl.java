d32f731ec50b7f0331b1019e3ee4914100e628f9
dongkyoon <dkyoon911@naver.com>
2025-08-04 14:18:08 +0900

    관리자 회원수정 완료

--- petmillie23/src/main/java/com/petmillie/admin/member/controller/AdminMemberControllerImpl.java
+++ petmillie23/src/main/java/com/petmillie/admin/member/controller/AdminMemberControllerImpl.java
@@ -7,8 +7,12 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,7 +28,7 @@ import com.petmillie.member.vo.MemberVO;
 public class AdminMemberControllerImpl extends BaseController  implements AdminMemberController{
 	@Autowired
 	private AdminMemberService adminMemberService;
-	
+
 	@RequestMapping(value="/adminMemberMain.do" ,method={RequestMethod.POST,RequestMethod.GET})
 	public ModelAndView adminGoodsMain(@RequestParam Map<String, String> dateMap,
 	                                   HttpServletRequest request, HttpServletResponse response)  throws Exception{
@@ -36,14 +40,14 @@ public class AdminMemberControllerImpl extends BaseController  implements AdminM
 	    String section = dateMap.get("section");
 	    String pageNum = dateMap.get("pageNum");
 	    String beginDate=null,endDate=null;
-	    
+
 	    String [] tempDate=calcSearchPeriod(fixedSearchPeriod).split(",");
 	    beginDate=tempDate[0];
 	    endDate=tempDate[1];
 	    dateMap.put("beginDate", beginDate);
 	    dateMap.put("endDate", endDate);
-	    
-	    
+
+
 	    HashMap<String,Object> condMap=new HashMap<String,Object>();
 	    if(section== null) {
 	        section = "1";
@@ -63,7 +67,7 @@ public class AdminMemberControllerImpl extends BaseController  implements AdminM
 
 	    ArrayList<MemberVO> member_list=adminMemberService.listMember(condMap);
 	    mav.addObject("member_list", member_list);
-	    
+
 	    String beginDate1[]=beginDate.split("-");
 	    String endDate2[]=endDate.split("-");
 	    mav.addObject("beginYear",beginDate1[0]);
@@ -72,7 +76,7 @@ public class AdminMemberControllerImpl extends BaseController  implements AdminM
 	    mav.addObject("endYear",endDate2[0]);
 	    mav.addObject("endMonth",endDate2[1]);
 	    mav.addObject("endDay",endDate2[2]);
-	    
+
 	    mav.addObject("section", section);
 	    mav.addObject("pageNum", pageNum);
 	    return mav;
@@ -88,56 +92,59 @@ public class AdminMemberControllerImpl extends BaseController  implements AdminM
 		mav.addObject("member_info",member_info);
 		return mav;
 	}
-	
+
 	@RequestMapping(value="/modifyMemberInfo.do" ,method={RequestMethod.POST,RequestMethod.GET})
 	public void modifyMemberInfo(HttpServletRequest request, HttpServletResponse response)  throws Exception{
-		request.setCharacterEncoding("utf-8");
-		response.setContentType("text/plain; charset=utf-8");
-		HashMap<String,String> memberMap=new HashMap<String,String>();
-		String val[]=null;
-		PrintWriter pw =response.getWriter();
-		String member_id=request.getParameter("member_id");
-		String mod_type=request.getParameter("mod_type");
-		String value =request.getParameter("value");
-		
-		if(mod_type.equals("member_pw")) {
-			memberMap.put("member_pw", value);
-		} else if(mod_type.equals("member_gender")) { // 성별 수정 로직 추가
-			memberMap.put("member_gender", value);
-		} else if(mod_type.equals("member_birth")){
-			val=value.split(",");
-			memberMap.put("member_birth_y",val[0]);
-			memberMap.put("member_birth_m",val[1]);
-			memberMap.put("member_birth_d",val[2]);
-			memberMap.put("member_birth_gn",val[3]);
-		} else if(mod_type.equals("tel")){
-			val=value.split(",");
-			memberMap.put("tel1",val[0]);
-			memberMap.put("tel2",val[1]);
-			memberMap.put("tel3",val[2]);
-		} else if(mod_type.equals("smssts_yn")){ // JSP에서 수정한 smssts_yn에 맞게 수정
-			memberMap.put("smssts_yn", value);
-		} else if(mod_type.equals("email")){ // 이메일 수정 로직 추가
-			val=value.split(",");
-			memberMap.put("email1",val[0]);
-			memberMap.put("email2",val[1]);
-			memberMap.put("emailsts_yn", val[2]);
-		} else if(mod_type.equals("address")){
-			val=value.split(",");
-			memberMap.put("zipcode",val[0]);
-			memberMap.put("roadAddress",val[1]);
-			memberMap.put("jibunAddress", val[2]);
-			memberMap.put("namujiAddress", val[3]);
-		}
-		
-		memberMap.put("member_id", member_id);		
-		
-		System.out.println("mod_type: " + mod_type);
-		System.out.println("value: " + value);
-		
-		adminMemberService.modifyMemberInfo(memberMap);
-		pw.print("mod_success");
-		pw.close();		
+	    request.setCharacterEncoding("utf-8");
+	    response.setContentType("text/plain; charset=utf-8");
+
+	    HashMap<String,String> memberMap=new HashMap<String,String>();
+	    String val[]=null;
+
+	    PrintWriter pw = response.getWriter();
+	    String member_id = request.getParameter("member_id");
+	    String mod_type = request.getParameter("mod_type");
+	    String value = request.getParameter("value");
+
+	    if(mod_type.equals("member_pw")) {
+	        memberMap.put("member_pw", value);
+	    } else if(mod_type.equals("member_gender")) {
+	        memberMap.put("member_gender", value);
+	    } else if(mod_type.equals("member_birth")){
+	        val=value.split(",");
+	        memberMap.put("member_birth_y",val[0]);
+	        memberMap.put("member_birth_m",val[1]);
+	        memberMap.put("member_birth_d",val[2]);
+	        memberMap.put("member_birth_gn",val[3]);
+	    } else if(mod_type.equals("tel")){
+	        val=value.split(",");
+	        memberMap.put("tel1",val[0]);
+	        memberMap.put("tel2",val[1]);
+	        memberMap.put("tel3",val[2]);
+	        memberMap.put("smssts_yn", val[3]);
+	    } else if(mod_type.equals("email")){
+	        val=value.split(",");
+	        memberMap.put("email1",val[0]);
+	        memberMap.put("email2",val[1]);
+	        memberMap.put("emailsts_yn", val[2]);
+	    } else if(mod_type.equals("address")){
+	        val=value.split(",");
+	        memberMap.put("zipcode",val[0]);
+	        memberMap.put("roadAddress",val[1]);
+	        memberMap.put("jibunAddress", val[2]);
+	        memberMap.put("namujiAddress", val[3]);
+	    } else {
+	        memberMap.put(mod_type, value);
+	    }
+
+	    memberMap.put("member_id", member_id);
+
+	    System.out.println("mod_type: " + mod_type);
+	    System.out.println("value: " + value);
+
+	    adminMemberService.modifyMemberInfo(memberMap);
+	    pw.print("mod_success");
+	    pw.close();
 	}
 	
 	@RequestMapping(value="/deleteMember.do" ,method={RequestMethod.POST})
@@ -148,10 +155,10 @@ public class AdminMemberControllerImpl extends BaseController  implements AdminM
 		String del_yn=request.getParameter("del_yn");
 		memberMap.put("del_yn", del_yn);
 		memberMap.put("member_id", member_id);
-		
+
 		adminMemberService.modifyMemberInfo(memberMap);
 		mav.setViewName("redirect:/admin/member/adminMemberMain.do");
 		return mav;
 	}
-		
+
 }
