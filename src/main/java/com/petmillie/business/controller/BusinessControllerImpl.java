c806372328778f2fe3965e1748dd49122129af01
ziapark <ziapark320@gmail.com>
2025-09-02 09:59:40 +0900

    되려나?

--- src/main/java/com/petmillie/business/controller/BusinessControllerImpl.java
+++ src/main/java/com/petmillie/business/controller/BusinessControllerImpl.java
@@ -423,7 +423,7 @@ public class BusinessControllerImpl extends BaseController implements BusinessCo
 	        redirectAttributes.addFlashAttribute("message", "객실 등록이 완료되었습니다.");
 	        // mypension.do 로 p_num을 보내줘야 해당 펜션의 객실 목록을 볼 수 있습니다.
 	        redirectAttributes.addAttribute("p_num", roomVO.getP_num()); 
-	        return "redirect:/business/mypension.do";
+	        return "redirect:/business/mypension.do?business_id=${businessInfo.business_id}";
 	        
 	    } catch (Exception e) {
 	        e.printStackTrace();
